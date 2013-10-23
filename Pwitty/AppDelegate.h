df389d94ec1c3065c90004d2fdcbeafbea5d3710
Nazwan <nazwan@Nazwans-MacBook-Pro.local>
2013-10-23 22:45:53 +0800

    first commit

---
+++ Pwitty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pwitty
+//
+//  Created by Nazwan on 10/23/13.
+//  Copyright (c) 2013 Nazwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PwittyTests/PwittyTests.m
@@ -0,0 +1,34 @@
+//
+//  PwittyTests.m
+//  PwittyTests
+//
+//  Created by Nazwan on 10/23/13.
+//  Copyright (c) 2013 Nazwan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PwittyTests : XCTestCase
+
+@end
+
+@implementation PwittyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
